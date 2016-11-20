8396a09d55b42a56d7e07265e0886af6fe319782
AdiA96 <adi.arnautovic@stu.ibu.edu.ba>
2016-11-20 22:35:10 +0100

    Addition to course class

--- src/ba/ibu/edu/sis/Course.java
+++ src/ba/ibu/edu/sis/Course.java
@@ -2,6 +2,8 @@ package ba.ibu.edu.sis;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import ba.ibu.edu.sis.StudentInformationSystem;
 
 public class Course {
@@ -24,6 +26,68 @@ public class Course {
 		
 		public void add(Course c){
 			
+		}
+		
+		double presentationPoints,midtermExam, finalExam; //No need to calculate percentage here cause points are always out of 100
+		int numOfHomeworks;
+		int numOfQuizzes;
+		int numOfLectures;
+		double homework[]=new double[numOfHomeworks];
+		double quizzes[]=new double[numOfQuizzes];
+		double attendence[]=new double[numOfLectures];
+		double obtainedHwPoints,obtainedQzPoints,obtainedAttendence;
+		
+		public void setNumOfHw(int numOfHw){
+			this.numOfHomeworks=numOfHw;
+		}
+		
+		public void setNumOfQuizzes(int numOfQz){
+			this.numOfQuizzes=numOfQz;
+		}
+		
+		public void setNumOfLectures(int numOfLectures){
+			this.numOfLectures=numOfLectures;
+		}
+		
+		public void showStudentsGrades(){
+			
+			//to get percentage of homework points
+			obtainedHwPoints=calculatePercentage(homework, numOfHomeworks);
+			//to get percentage of quizzes points
+			obtainedQzPoints=calculatePercentage(quizzes, numOfQuizzes);
+			//to get percentage of attendence
+			obtainedAttendence=calculatePercentage(attendence, numOfLectures);
+			
+			JOptionPane.showMessageDialog(null, "Presentation:" + presentationPoints + "%" + "\n" + "Midterm: " + midtermExam + "%" + "\n" + "Final: " + finalExam + "%" + "\n" + "Homework: " + obtainedHwPoints + "%" + "\n" + "Quizzes: " + obtainedQzPoints + "%" + "\n" + "Attendence: " + obtainedAttendence + "%");
+		
+		}
+		
+		private double calculatePercentage(double particle[], int numOfParticles)
+		{
+			double result = 0,obtainedPoints;
+			int maximumPoints;
+			
+			for(int i=0;i<numOfParticles;i++)
+			{
+				result+=particle[i];	
+			}
+			maximumPoints=100*numOfParticles;
+			obtainedPoints=result/maximumPoints*100;
+			
+			return obtainedPoints;
+			
+		}
+		
+		public void setHomeworkResult(int numOfHw, double numOfPoints){
+			this.homework[numOfHw-1]=numOfPoints;
+		}
+		
+		public void setQuizResult(int numOfQz, double numOfPoints){
+			this.quizzes[numOfQz-1]=numOfPoints;
+		}
+		
+		public void setAttendence(int numOfLecture){
+			this.attendence[numOfLecture]=100;
 		}
 }
 
