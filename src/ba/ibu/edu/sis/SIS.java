fd4550e925d4f80751bd671406b3efb8d04f6f9f
Armin <Armin@DESKTOP-BLNTCQT>
2016-11-21 15:55:01 +0100

    Class professor start

--- src/ba/ibu/edu/sis/SIS.java
+++ src/ba/ibu/edu/sis/SIS.java
@@ -117,7 +117,45 @@ public class SIS {
 			this.grades = grades;
 		}
 	}
-
+	
+	static class Professor {
+		private String ID;
+		private String name;
+		private String surname;
+		private String email;
+		private String phone;
+		public String getID() {
+			return ID;
+		}
+		public void setID(String iD) {
+			ID = iD;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getSurname() {
+			return surname;
+		}
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		
+	}
 	public static void main(String[] args) {
 
 
